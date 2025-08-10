4b40d26ba1e4dade8b3aa0bcddd9ecbbf1e2f8ce
Abhishek Jain <abhishekjai35@gmail.com>
2025-08-10 20:28:50 +0530

    CLI tests (cli-test-1754837930488)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket  # noqa: F401
+import threading
 
 
 def main():
@@ -8,11 +9,20 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() # wait for client
+     # wait for client
 
     while True:
-        connection.recv(1024)  # receive data from client
-        connection.sendall(b"+PONG\r\n")
+        connection, _ = server_socket.accept()
+        thread = threading.Thread(target=handle_client, args=(connection,))
+        thread.start()
+
+
+def handle_client(connection):
+    while True:
+        data = connection.recv(1024)  # receive data from client
+        if not data:
+            break
+        connection.sendall(b"+PONG\r\n")  # send response to client
 
 
 if __name__ == "__main__":
