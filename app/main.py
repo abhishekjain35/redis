1d814d8331f6690ed01174715ae628bb34b3fa56
Abhishek Jain <abhishekjai35@gmail.com>
2025-08-10 20:06:30 +0530

    CLI tests (cli-test-1754836590908)

--- app/main.py
+++ app/main.py
@@ -9,7 +9,11 @@ def main():
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept() # wait for client
-    connection.sendall(b"+PONG\r\n")
+
+    while True:
+        data = connection.recv(1024)  # receive data from client
+        print(f"Received data: {data!r}")
+        connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
